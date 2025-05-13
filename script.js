079163fab4f160936febc9c3abf6cef5255683f2
Kartik Singh <167749570+KartikSingh09@users.noreply.github.com>
2025-05-13 13:31:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function stringChop(str, size) {
-  // your code here
-}
 
-// Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+function chunkString(str, chunkLength) {
+    if (!str) return []; // Return [] if str is null or undefined
+
+    chunkLength = Number(chunkLength);
+    if (isNaN(chunkLength) || chunkLength <= 0) return [];
+
+    const chunks = [];
+
+    for (let i = 0; i < str.length; i += chunkLength) {
+        chunks.push(str.substring(i, i + chunkLength));
+    }
+
+    return chunks;
+}
